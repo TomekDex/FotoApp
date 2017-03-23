a098b278239caa282d329552c83751c485025506
TomekDex <tjtj@poczta.onet.pl>
2017-03-23 18:51:33 +0100

    Start

---
+++ FotoApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace FotoApp.ViewModels
+{
+    class MainViewModel : PropertyChangedBase
+    {
+        private const string WindowTitleDefault = "FotoApp";
+        private string _windowTitle = WindowTitleDefault;
+        public string WindowTitle
+        {
+            get { return _windowTitle; }
+            set
+            {
+                _windowTitle = value;
+                NotifyOfPropertyChange(() => WindowTitle);
+            }
+        }
+    }
+}
