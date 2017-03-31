7aaeeeb2459a045acf8c4216d7dc445426ff5cb0
mecenasy <mecenasy@gmail.com>
2017-03-31 19:38:34 +0200

    Dodano ListFotoView do wyświetlania zdięć ale jeszcze nie podłaczone

---
+++ FotoApp/Models/Data.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace FotoApp.Models
+{
+    public class Data
+    {
+        public int index { get; set; }
+        public BitmapImage bitmap { get; set; }
+        public bool Chekerd { get; set; }
+
+        public void Cos()
+        {
+            
+        }
+        
+    }
+}
