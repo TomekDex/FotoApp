5cacb1cdc941744d3484afeec555e438f0aa10f9
Tomasz Juszczak <tjtj@poczta.onet.pl>
2017-04-12 14:12:00 +0200

    Revert "Revert "Porawa walidacji TextBox""
    This reverts commit 9d9d4772ad294efa58eaf0c98078d3316a7491b3.

--- FotoApp/ViewModels/ListFotoViewModel.cs
+++ FotoApp/ViewModels/ListFotoViewModel.cs
@@ -2,24 +2,23 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Media.Imaging;
 using Caliburn.Micro;
-using FotoApp.Interface;
-using FotoApp.Models;
 using FotoApp.Models.ChangePapersAnSiseModel;
 using FotoApp.Models.FotoColection;
-using FotoApp.Schell;
 using FotoApp.ViewModels.Actions;
 using FotoApp.ViewModels.EvenArgs;
 
 namespace FotoApp.ViewModels
 {
-    public class ListFotoViewModel : ViewModelBase.ViewModelBase, IHandle<IEnumerable<object>>
+    public class ListFotoViewModel : ViewModelBase.ViewModelBase//, IHandle<IEnumerable<object>>
     {
         
         private readonly GetFotoViewModel _getFoto;
+
+        public delegate void GetPaperDelegate();
+
+        public event GetPaperDelegate getPaperDelegete = null;
 
         #region Propertis;
 
@@ -27,7 +26,7 @@ namespace FotoApp.ViewModels
 
         public BindableCollection<Foto> FotoData
         {
-            get => _fotoData;
+            get{return _fotoData;}
             set
             {
                 _fotoData = value;
@@ -35,8 +34,17 @@ namespace FotoApp.ViewModels
             }
         }
 
-        private readonly FinalFotoColection _finalColections;
+        public int Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        public Sizes Sise {
+            get { return _sise; }
+            set { _sise = value; }
+        }
 
+        private FinalFotoColection _finalColections;
         private int _type;
         private Sizes _sise;
 
@@ -49,7 +57,8 @@ namespace FotoApp.ViewModels
         {
             _getFoto = getFoto;
             EventAggregator = eventAggregator;
-            EventAggregator.Subscribe(this);
+            //EventAggregator.Subscribe(this);
+            getPaperDelegete?.Invoke();
             _finalColections = new FinalFotoColection();
             _getFoto.FinalColectionDelegat += GetFinalColection;
 
@@ -239,8 +248,8 @@ namespace FotoApp.ViewModels
                     Index = tmp.Index,
                     FullPathOfFoto = uri.ToString(),
                     NameOfFoto = fileName,
-                    Type = _type,
-                    Size = _sise
+                    Type = Type,
+                    Size = Sise
                 };
                 _finalColections.FotoColection.Add(foto);
                 EventAggregator.PublishOnCurrentThread(true);
@@ -265,15 +274,18 @@ namespace FotoApp.ViewModels
             tmp.GetFotoColection(_getFoto, _finalColections);
         }
 
-        public void Handle(IEnumerable<object> message)
-        {
-            var list = message.ToList();
-            if (list != null)
-            {
-                _type = (int)list[0];
-                _sise = list[1] as Sizes;
-            }
-        }
+        //public void Handle(IEnumerable<object> message)
+        //{
+        //    var list = message.ToList();
+        //    if (list != null)
+        //    {
+        //        Type = (int)list[0];
+        //        Sise = list[1] as Sizes;
+        //    }
+        //}
+
+        
+
         #endregion
     }
 }
