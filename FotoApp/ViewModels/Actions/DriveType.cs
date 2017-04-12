5a5d6c334e6929b08aaee711d35da9637c3e66d0
KAMIKINGDOM\Kami in Black <grabowska.joanna4@gmail.com>
2017-04-12 13:33:25 +0200

    Metoda zwracająca typy podłączonych dysków

---
+++ FotoApp/ViewModels/Actions/DriveType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoApp.ViewModels.Actions
+{
+    public class DriveType
+    {
+        public DriveType() 
+        {
+            GetDriveType(); //zwraca wszystkie dyski (nazwa i typ) podłączone do komputera w formie klasy DriveInfo
+        }
+
+        public DriveInfo[] GetDriveType()
+        {
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+
+           return allDrives;
+        }
+
+    }
+}
