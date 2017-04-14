07f97ca97f2a6a242a4e2068695aa129049f7d8d
KAMIKINGDOM\Kami in Black <grabowska.joanna4@gmail.com>
2017-04-14 20:43:52 +0200

    Ulepszyłam metodę getdrive

--- FotoApp/ViewModels/Actions/DriveType.cs
+++ FotoApp/ViewModels/Actions/DriveType.cs
@@ -12,14 +12,21 @@ namespace FotoApp.ViewModels.Actions
         public DriveType() 
         {
             GetDriveType(); //zwraca wszystkie dyski (nazwa i typ) podłączone do komputera w formie klasy DriveInfo
+
+
         }
 
-        public DriveInfo[] GetDriveType()
+        static Caliburn.Micro.BindableCollection<DriveInfo> GetDriveType()
         {
-            DriveInfo[] allDrives = DriveInfo.GetDrives();
-
-           return allDrives;
+            Caliburn.Micro.BindableCollection<DriveInfo> d = new Caliburn.Micro.BindableCollection<DriveInfo>();
+            DriveInfo[] loadedDrives = DriveInfo.GetDrives();
+            foreach (var loadedDrive in loadedDrives)
+            {
+                if (loadedDrive.IsReady == true && (loadedDrive.DriveType == System.IO.DriveType.Removable || loadedDrive.DriveType == System.IO.DriveType.CDRom))
+                    d.Add(loadedDrive);
+            }
+            return d;
         }
-
+               
     }
 }
