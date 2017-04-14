396c728fb113fde634f18c66ab97a4b1a9cfba91
KAMIKINGDOM\Kami in Black <grabowska.joanna4@gmail.com>
2017-04-14 22:55:37 +0200

    Dodana Klasa Nowy_Katalog

---
+++ FotoApp/ViewModels/Actions/NewDirectory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoApp.ViewModels.Actions
+{
+    public class NewDirectory
+    {
+
+        string path, directoryname;
+
+        public NewDirectory()
+        {
+
+            CreateDirectory(path,directoryname);
+            
+        }
+
+        public string CreateDirectory(string path, string directoryname)
+        {
+
+           string newpath = path + directoryname;
+           string nameDirectory = Directory.CreateDirectory(newpath).ToString();
+
+            return nameDirectory;
+        }
+        
+    }
+}
