7ea0c319014e42e93469e8fd9104d45477a0da6f
mecenasy <mecenasy@gmail.com>
2017-05-02 20:37:20 +0200

    drobne zmiany

--- FotoApp/Models/FotoColection/Foto.cs
+++ FotoApp/Models/FotoColection/Foto.cs
@@ -1,13 +1,31 @@
-﻿using System.Drawing;
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
 using System.Windows.Media.Imaging;
+using FotoApp.Annotations;
 
 namespace FotoApp.Models.FotoColection
 {
-    public class Foto
+    public class Foto : INotifyPropertyChanged
     {
+        private BitmapImage _bitmap;
         public int Index { get; set; }
         public string path { get; set; }
-        public BitmapImage bitmap { get; set; }
+        public BitmapImage bitmap
+        {
+            get => _bitmap;
+            set
+            {
+                _bitmap = value;
+                OnPropertyChanged(nameof(bitmap));
+            } }
         public bool Chekerd { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- FotoApp/Models/FotoColection/OrderFoto.cs
+++ FotoApp/Models/FotoColection/OrderFoto.cs
@@ -1,16 +1,54 @@
-﻿using System.Windows.Media.Imaging;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media.Imaging;
+using FotoApp.Annotations;
 using FotoAppDB.DBModel;
 
 namespace FotoApp.Models.FotoColection
 {
-    public class OrderFoto
+    public class OrderFoto: INotifyPropertyChanged
     {
+        private string _size;
+        private string _type;
+        private int _quantity;
         public Fotos Foto { get; set; }
-        public string Size { get; set; }
-        public string Type { get; set; }
-        public int Quantity { get; set; }
+        public string Size {
+            get => _size;
+            set
+            {
+                _size = value;
+                OnPropertyChanged(nameof(Size));
+            } }
+
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                _type = value;
+                OnPropertyChanged(nameof(Type));
+            }
+        }
+
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                _quantity = value;
+                OnPropertyChanged(nameof(Quantity));
+            }
+        }
+
         public BitmapImage Image { get; set; }
         public Papers Paper { get; set; }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
